3875a5e9e2408caf0015cc26aa681a32bf858e50
Vinhuanc <huann.chenn@gmail.com>
2022-09-30 19:24:19 -0500

    finished with Assignment06-Program01

--- Assignment06/main.cpp
+++ Assignment06/main.cpp
@@ -6,20 +6,28 @@ using namespace std;
 
 int main(){
     ifstream inputFile;
-    int numbers;
-
-    inputFile.open("/Users/hchen24/Desktop/numbers.txt");
+    double numbers, total=0, count;
+    
+    const string fileName = "/Users/hchen24/Desktop/numbers.txt";
+    inputFile.open(fileName);
   
     cout << "opening file"<<endl;
     if(inputFile){
         cout << "*******  Data from the file:  *******" <<endl;
     while(inputFile >> numbers){
         cout << numbers<< endl;
+        total+=numbers;
+        count++;
     }
     inputFile.close();
     }else{
         cout<< "Error opening the file.";
         return 0;
     }
+    double avg=total/count;
+    cout<<"------------------------"<<"\n";
+    cout <<"The sum of all the numbers in the file: "<< total<<"\n";
+    cout << "The number of numbers in a file: "<< count<<"\n";
+    cout << "The average of all the numbers in the file: "<<avg<<"\n";
 
 }
